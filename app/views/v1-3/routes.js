a843b2c316b3cf6d4f6fd106e150e99d914f9f39
Nick Dunn <nick.dunn@digital.hmrc.gov.uk>
2020-04-14 14:19:23 +0100

    Confirmation content

--- app/views/v1-3/routes.js
+++ app/views/v1-3/routes.js
@@ -1,4 +1,4 @@
-const url = require('url')
+// const url = require('url')
 
 module.exports = function (router) {
   // set the sprint folder name as a variable
@@ -6,40 +6,38 @@ module.exports = function (router) {
 
   // route-claim date
   router.post('/' + sprint + '/route-claim-start', function (req, res) {
-    req.session.data.claimStart = req.session.data.claimPeriodStartDay + "/" + req.session.data.claimPeriodStartMonth + "/" + req.session.data.claimPeriodStartYear;
-    var titleMonth = Math.round(req.session.data.claimPeriodStartMonth);
+    req.session.data.claimStart = req.session.data.claimPeriodStartDay + '/' + req.session.data.claimPeriodStartMonth + '/' + req.session.data.claimPeriodStartYear
+    var titleMonth = Math.round(req.session.data.claimPeriodStartMonth)
     if (titleMonth === 2) {
-      req.session.data.claimPeriodStartMonthTitle = ' February';
+      req.session.data.claimPeriodStartMonthTitle = ' February'
     } else if (titleMonth === 3) {
-      req.session.data.claimPeriodStartMonthTitle = ' March';
+      req.session.data.claimPeriodStartMonthTitle = ' March'
     } else if (titleMonth === 4) {
-      req.session.data.claimPeriodStartMonthTitle = ' April';
+      req.session.data.claimPeriodStartMonthTitle = ' April'
     } else if (titleMonth === 5) {
-      req.session.data.claimPeriodStartMonthTitle = ' May';
+      req.session.data.claimPeriodStartMonthTitle = ' May'
     }
 
-    req.session.data.payClaimPeriodTitle = Math.round(req.session.data.claimPeriodStartDay) + req.session.data.claimPeriodStartMonthTitle;
-
-
+    req.session.data.payClaimPeriodTitle = Math.round(req.session.data.claimPeriodStartDay) + req.session.data.claimPeriodStartMonthTitle
 
     res.redirect('/' + sprint + '/claim-period-1')
   })
 
   // route-claim date
   router.post('/' + sprint + '/route-claim-end', function (req, res) {
-    req.session.data.claimEnd = req.session.data.claimPeriodEndDay + "/" + req.session.data.claimPeriodEndMonth + "/" + req.session.data.claimPeriodEndYear;
-    var titleMonth = Math.round(req.session.data.claimPeriodEndMonth);
+    req.session.data.claimEnd = req.session.data.claimPeriodEndDay + '/' + req.session.data.claimPeriodEndMonth + '/' + req.session.data.claimPeriodEndYear
+    var titleMonth = Math.round(req.session.data.claimPeriodEndMonth)
     if (titleMonth === 2) {
-      req.session.data.claimPeriodEndMonthTitle = ' February';
+      req.session.data.claimPeriodEndMonthTitle = ' February'
     } else if (titleMonth === 3) {
-      req.session.data.claimPeriodEndMonthTitle = ' March';
+      req.session.data.claimPeriodEndMonthTitle = ' March'
     } else if (titleMonth === 4) {
-      req.session.data.claimPeriodEndMonthTitle = ' April';
+      req.session.data.claimPeriodEndMonthTitle = ' April'
     } else if (titleMonth === 5) {
-      req.session.data.claimPeriodEndMonthTitle = ' May';
+      req.session.data.claimPeriodEndMonthTitle = ' May'
     }
 
-    req.session.data.payClaimPeriodEndTitle = Math.round(req.session.data.claimPeriodEndDay) + req.session.data.claimPeriodEndMonthTitle;
+    req.session.data.payClaimPeriodEndTitle = Math.round(req.session.data.claimPeriodEndDay) + req.session.data.claimPeriodEndMonthTitle
     res.redirect('/' + sprint + '/furlough-question')
   })
 
@@ -52,15 +50,12 @@ module.exports = function (router) {
     } else {
       res.redirect('/' + sprint + '/furlough-dates')
     }
-
   })
 
   // route - pay dates
   router.post('/' + sprint + '/route-pay-question', function (req, res) {
     res.redirect('/' + sprint + '/pay-frequency')
   })
-
-
 
   // route-pay frequency
   router.post('/' + sprint + '/route-frequency', function (req, res) {
@@ -69,7 +64,6 @@ module.exports = function (router) {
       req.session.data.payFrequency = 'each month'
     } else if (data === '4Weekly') {
       req.session.data.payFrequency = 'every 4 weeks'
-
     } else if (data === 'fortnightly') {
       req.session.data.payFrequency = 'every 2 weeks'
     } else if (data === 'weekly') {
@@ -86,43 +80,43 @@ module.exports = function (router) {
 
   // route - pay dates 1
   router.post('/' + sprint + '/route-pay-dates-1', function (req, res) {
-    var titleMonth = Math.round(req.session.data.payPeriodOneStartMonth);
+    var titleMonth = Math.round(req.session.data.payPeriodOneStartMonth)
     if (titleMonth === 2) {
-      req.session.data.payPeriodOneTitleMonth = ' February';
+      req.session.data.payPeriodOneTitleMonth = ' February'
     } else if (titleMonth === 3) {
-      req.session.data.payPeriodOneTitleMonth = ' March';
+      req.session.data.payPeriodOneTitleMonth = ' March'
     } else if (titleMonth === 4) {
-      req.session.data.payPeriodOneTitleMonth = ' April';
+      req.session.data.payPeriodOneTitleMonth = ' April'
     } else if (titleMonth === 5) {
-      req.session.data.payPeriodOneTitleMonth = ' May';
+      req.session.data.payPeriodOneTitleMonth = ' May'
     }
 
-    req.session.data.payPeriodOneTitle = Math.round(req.session.data.payPeriodOneStartDay) + req.session.data.payPeriodOneTitleMonth;
-    req.session.data.payPeriodOne = req.session.data.payPeriodOneStartDay + "/" + req.session.data.payPeriodOneStartMonth + "/" + req.session.data.payPeriodOneStartYear;
+    req.session.data.payPeriodOneTitle = Math.round(req.session.data.payPeriodOneStartDay) + req.session.data.payPeriodOneTitleMonth
+    req.session.data.payPeriodOne = req.session.data.payPeriodOneStartDay + '/' + req.session.data.payPeriodOneStartMonth + '/' + req.session.data.payPeriodOneStartYear
     res.redirect('/' + sprint + '/pay-dates-2')
   })
 
   // route - pay dates 2
   router.post('/' + sprint + '/route-pay-dates-2', function (req, res) {
-    var titleMonth = Math.round(req.session.data.payPeriodTwoStartMonth);
+    var titleMonth = Math.round(req.session.data.payPeriodTwoStartMonth)
     if (titleMonth === 2) {
-      req.session.data.payPeriodTwoTitleMonth = ' February';
+      req.session.data.payPeriodTwoTitleMonth = ' February'
     } else if (titleMonth === 3) {
-      req.session.data.payPeriodTwoTitleMonth = ' March';
+      req.session.data.payPeriodTwoTitleMonth = ' March'
     } else if (titleMonth === 4) {
-      req.session.data.payPeriodTwoTitleMonth = ' April';
+      req.session.data.payPeriodTwoTitleMonth = ' April'
     } else if (titleMonth === 5) {
-      req.session.data.payPeriodTwoTitleMonth = ' May';
+      req.session.data.payPeriodTwoTitleMonth = ' May'
     }
 
-    req.session.data.payPeriodTwoTitle = Math.round(req.session.data.payPeriodTwoStartDay) + req.session.data.payPeriodTwoTitleMonth;
-   req.session.data.payPeriodTwo = req.session.data.payPeriodTwoStartDay + "/" + req.session.data.payPeriodTwoStartMonth + "/" + req.session.data.payPeriodTwoStartYear;
+    req.session.data.payPeriodTwoTitle = Math.round(req.session.data.payPeriodTwoStartDay) + req.session.data.payPeriodTwoTitleMonth
+    req.session.data.payPeriodTwo = req.session.data.payPeriodTwoStartDay + '/' + req.session.data.payPeriodTwoStartMonth + '/' + req.session.data.payPeriodTwoStartYear
     res.redirect('/' + sprint + '/pay-dates-3')
   })
 
   // route - pay dates 3
   router.post('/' + sprint + '/route-pay-dates-3', function (req, res) {
-    req.session.data.payPeriodThree = req.session.data.payPeriodThreeStartDay + "/" + req.session.data.payPeriodThreeStartMonth + "/" + req.session.data.payPeriodThreeStartYear;
+    req.session.data.payPeriodThree = req.session.data.payPeriodThreeStartDay + '/' + req.session.data.payPeriodThreeStartMonth + '/' + req.session.data.payPeriodThreeStartYear
     res.redirect('/' + sprint + '/nic-category')
   })
 
@@ -131,18 +125,16 @@ module.exports = function (router) {
     var data = req.session.data.anotherDate
     if (data === 'yes') {
       if (req.session.data.extraPayPeriodOne === 'true') {
-        req.session.data.extraPayPeriodTwo = 'true';
+        req.session.data.extraPayPeriodTwo = 'true'
         res.redirect('/' + sprint + '/pay-dates-3')
       } else {
-        req.session.data.extraPayPeriodOne = 'true';
+        req.session.data.extraPayPeriodOne = 'true'
         res.redirect('/' + sprint + '/pay-dates-2')
       }
     } else {
       res.redirect('/' + sprint + '/furlough-question')
     }
   })
-
-
 
   // route - nic category
   router.post('/' + sprint + '/route-nic', function (req, res) {
@@ -164,62 +156,59 @@ module.exports = function (router) {
       req.session.data.pensionStatus = 'opted into pension auto-enrolment'
     }
 
-  // multiply for total
+    // multiply for total
     if (req.session.data.payPeriodOne && req.session.data.payPeriodTwo && req.session.data.payPeriodThree) {
-      req.session.data.furloughTotalCalc = 3;
-    } else if (req.session.data.payPeriodOne && req.session.data.payPeriodTwo ) {
-      req.session.data.furloughTotalCalc = 2;
+      req.session.data.furloughTotalCalc = 3
+    } else if (req.session.data.payPeriodOne && req.session.data.payPeriodTwo) {
+      req.session.data.furloughTotalCalc = 2
     } else {
-      req.session.data.furloughTotalCalc = 1;
+      req.session.data.furloughTotalCalc = 1
     }
 
-
     // Daily pay calc
-    req.session.data.payDaily = Math.round (req.session.data.salaryAmount / 20);
+    req.session.data.payDaily = Math.round(req.session.data.salaryAmount / 20)
     // req.session.data.payPeriodNic = Math.round (req.session.data.payPeriodFurloughSalary * 0.12);
     // req.session.data.payPeriodPension = Math.round (req.session.data.payPeriodNic * 0.43);
 
     //  pay period one calc
-    req.session.data.payPeriodOneFurloughSalary = Math.round ((req.session.data.payDaily * 17) * 0.8);
-    req.session.data.payPeriodOneNic = Math.round (req.session.data.payPeriodOneFurloughSalary * 0.12);
-    req.session.data.payPeriodOnePension = Math.round (req.session.data.payPeriodOneNic * 0.43);
+    req.session.data.payPeriodOneFurloughSalary = Math.round((req.session.data.payDaily * 17) * 0.8)
+    req.session.data.payPeriodOneNic = Math.round(req.session.data.payPeriodOneFurloughSalary * 0.12)
+    req.session.data.payPeriodOnePension = Math.round(req.session.data.payPeriodOneNic * 0.43)
     //  pay period two calc
-    req.session.data.payPeriodTwoFurloughSalary = Math.round ((req.session.data.payDaily * 13) * 0.8);
-    req.session.data.payPeriodTwoNic = Math.round (req.session.data.payPeriodTwoFurloughSalary * 0.12);
-    req.session.data.payPeriodTwoPension = Math.round (req.session.data.payPeriodTwoNic * 0.43);
+    req.session.data.payPeriodTwoFurloughSalary = Math.round((req.session.data.payDaily * 13) * 0.8)
+    req.session.data.payPeriodTwoNic = Math.round(req.session.data.payPeriodTwoFurloughSalary * 0.12)
+    req.session.data.payPeriodTwoPension = Math.round(req.session.data.payPeriodTwoNic * 0.43)
 
     // set the totals
-    req.session.data.totalFurlough = req.session.data.payPeriodOneFurloughSalary + req.session.data.payPeriodTwoFurloughSalary;
-    req.session.data.totalNic = req.session.data.payPeriodOneNic + req.session.data.payPeriodTwoNic ;
-    req.session.data.totalPension = req.session.data.payPeriodOnePension + req.session.data.payPeriodTwoPension;
+    req.session.data.totalFurlough = req.session.data.payPeriodOneFurloughSalary + req.session.data.payPeriodTwoFurloughSalary
+    req.session.data.totalNic = req.session.data.payPeriodOneNic + req.session.data.payPeriodTwoNic
+    req.session.data.totalPension = req.session.data.payPeriodOnePension + req.session.data.payPeriodTwoPension
 
     res.redirect('/' + sprint + '/tax-year-pay-date')
   })
 
   // route - nic category
   router.post('/' + sprint + '/route-tax-pay-date', function (req, res) {
-    req.session.data.payTaxDate = req.session.data.payDateDay + "/" + req.session.data.payDateMonth + "/" + req.session.data.payDateYear;
-    var titleMonth = Math.round(req.session.data.payDateMonth);
+    req.session.data.payTaxDate = req.session.data.payDateDay + '/' + req.session.data.payDateMonth + '/' + req.session.data.payDateYear
+    var titleMonth = Math.round(req.session.data.payDateMonth)
     if (titleMonth === 2) {
-      req.session.data.payDateMonthTitle = ' February';
+      req.session.data.payDateMonthTitle = ' February'
     } else if (titleMonth === 3) {
-      req.session.data.payDateMonthTitle = ' March';
+      req.session.data.payDateMonthTitle = ' March'
     } else if (titleMonth === 4) {
-      req.session.data.payDateMonthTitle = ' April';
+      req.session.data.payDateMonthTitle = ' April'
     } else if (titleMonth === 5) {
-      req.session.data.payDateMonthTitle = ' May';
+      req.session.data.payDateMonthTitle = ' May'
     }
 
-    req.session.data.payTaxDateTitle = Math.round(req.session.data.payDateDay) + req.session.data.payDateMonthTitle;
+    req.session.data.payTaxDateTitle = Math.round(req.session.data.payDateDay) + req.session.data.payDateMonthTitle
     res.redirect('/' + sprint + '/confirmation')
   })
 
-  //clear data
+  // clear data
   router.post('/' + sprint + '/reset', function (req, res) {
-     req.session.data = {};
-     req.session.destroy();
-     res.redirect('/' + sprint + '/claim-period')
-
-   })
+    req.session.data = {}
+    req.session.destroy()
+    res.redirect('/' + sprint + '/claim-period')
+  })
 }
-
