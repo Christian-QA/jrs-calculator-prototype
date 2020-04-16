b39048a2aaa17184aa72e0c13be5c2799d9a848e
Nick Dunn <nick.dunn@digital.hmrc.gov.uk>
2020-04-16 11:14:05 +0100

    1.4

--- app/routes.js
+++ app/routes.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-require('./views/v1/routes')(router);
-require('./views/v1-1/routes')(router);
-require('./views/v1-2/routes')(router);
-require('./views/v1-3/routes')(router);
+require('./views/v1/routes')(router)
+require('./views/v1-1/routes')(router)
+require('./views/v1-2/routes')(router)
+require('./views/v1-3/routes')(router)
+require('./views/v1-4/routes')(router)
 
 module.exports = router
 
